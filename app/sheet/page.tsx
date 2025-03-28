ed02aa679879c8c41ed94f0d0efcdaf24efb3e7d
Bhupesh-Khordia <bhupeshy510@gmail.com>
2025-03-29 04:32:53 +0530

    Enhance Hero component with new call-to-action and add sheet page for ClientSpreadsheet

---
+++ app/sheet/page.tsx
@@ -0,0 +1,12 @@
+import ClientSpreadsheet from '@/components/ClientSpreadsheet'
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+      <ClientSpreadsheet />
+    </div>
+  )
+}
+
+export default page
--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,27 +1,36 @@
-import React from 'react';
-import { Layers, FileSpreadsheet } from 'lucide-react';
+import React from "react";
+import Link from "next/link";
 
 const Hero = () => {
-    return (
-        <div className="min-h-screen bg-gradient-to-br from-[#E0FFFF] via-[#F0F8FF] to-[#E6E6FA]">
-          {/* Hero Section */}
-          <div className="pt-32 pb-20 px-4 justify-center items-center flex flex-col min-h-screen">
-            <div className="max-w-4xl mx-auto text-center justify-center items-center flex flex-col">
-              <h1 className="text-5xl md:text-7xl font-bold text-gray-900 mb-8 leading-tight">
-                Real-Time Personal Finance Tracker
-              </h1>
-              
-              <p className="text-xl text-gray-600 mb-12 max-w-3xl mx-auto">
-                Track your spending, manage your budget, and achieve your financial goals with our intuitive and user-friendly platform.
-              </p>
-            </div>
-          </div>
-          <div>
-            
-          </div>
+  return (
+    <div className="min-h-screen bg-gradient-to-br from-[#E0FFFF] via-[#F0F8FF] to-[#E6E6FA]">
+      {/* Hero Section */}
+      <div className="pt-32 pb-20 px-4 justify-center items-center flex flex-col min-h-screen">
+        <div className="max-w-4xl mx-auto text-center justify-center items-center flex flex-col">
+          <h1 className="text-5xl md:text-7xl font-bold text-gray-900 mb-8 leading-tight">
+            Real-Time Personal Finance Tracker
+          </h1>
+
+          <p className="text-xl text-gray-600 mb-12 max-w-3xl mx-auto">
+            Track your spending, manage your budget, and achieve your financial
+            goals with our intuitive and user-friendly platform.
+          </p>
         </div>
+      </div>
+      <div className="min-h-screen justify-center flex flex-col items-center gap-8">
+        <p className="text-3xl md:text-5xl font-bold text-gray-900 mb-8 leading-tight text-center">
+          Create or Import your first expenses list today
+          <br /> and take control of your finances!
+        </p>
 
-    );
-}
+        <Link href="/sheet">
+          <button className="p-10 cursor-pointer bg-blue-500 text-white rounded-lg hover:bg-blue-600">
+            Get started Now
+          </button>
+        </Link>
+      </div>
+    </div>
+  );
+};
 
-export default Hero
+export default Hero;
